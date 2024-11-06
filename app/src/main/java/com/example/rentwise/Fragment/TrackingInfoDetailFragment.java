eb00a4d9345dd59e325e5de6d01984583ae95da1
Ddraigo <laithanh1000@gmai.com>
2024-11-07 00:33:37 +0700

    cập nhất thông tin thuê xe chi tiết

--- app/src/main/java/com/example/rentwise/Fragment/TrackingInfoDetailFragment.java
+++ app/src/main/java/com/example/rentwise/Fragment/TrackingInfoDetailFragment.java
@@ -1,28 +1,17 @@
 package com.example.rentwise.Fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.rentwise.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TrackingInfoDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TrackingInfoDetailFragment extends Fragment {
+public class TrackingInfoDetailFragment extends BottomSheetDialogFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +19,6 @@ public class TrackingInfoDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TrackingInfoDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static TrackingInfoDetailFragment newInstance(String param1, String param2) {
         TrackingInfoDetailFragment fragment = new TrackingInfoDetailFragment();
         Bundle args = new Bundle();
@@ -58,8 +38,7 @@ public class TrackingInfoDetailFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tracking_info_detail, container, false);
     }
